5904e6ed8df6cbec7d9287e48ed15c8880a24cb9
„Delakosta„ <lakosta4@gmail.com>
2024-02-19 22:05:18 +0100

    Laptop

---
+++ React/prvi-cas/src/CounterList.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import CounterListItem from "./CounterListItem";
+
+const CounterList = () => {
+    const [counters, setCounters] = useState([
+        {id: 1, value: 0},
+        {id: 2, value: 5},
+        {id: 3, value: 9},
+    ]);
+
+// const items = ['apple', 'banana', 'orange'];
+// const renderedItems = [];
+
+// items.forEach(item => {
+//     renderedItems.push(<li>{item}</li>);
+// });
+
+
+    return (
+        <div>
+            <h1>Counters List:</h1>
+            <ul>
+                {/* Array.map() - vraca transformisane elemente */}
+                {/* Array.forEach - simply iterates / ide kroz niz bez da vraca bilo sta */}
+                {/* renderedItems */}
+                {counters.map((counter) => {
+                    <li key={counter.id}>{counter.value}</li>
+                })}
+                {counters.map(counter => {
+                    return <div key={counter.id}>{counter.id}: <CounterListItem counter={counter} />
+
+                        </div>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default CounterList;
---
+++ React/prvi-cas/src/CounterListItem.js
@@ -0,0 +1,5 @@
+const CounterListItem = (props) => {
+    return <div>Counter List Item {props.counter.value}</div>
+}
+
+export default CounterListItem;
