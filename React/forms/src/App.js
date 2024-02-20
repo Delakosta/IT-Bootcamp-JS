7eb8467ff347c1093e03315dbe361f3aa5d506c3
„Delakosta„ <lakosta4@gmail.com>
2024-02-20 22:11:15 +0100

    Laptop

---
+++ React/forms/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import Textarea from './components/textArea';
+import Input from './components/input';
+import Select from './components/select';
+import Checkbox from './components/checkbox';
+import Vezba from './components/vezba';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Form />
+      <Textarea />
+      <Input />
+      <Select />
+      <Checkbox />*/}
+      <Vezba />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ React/forms/src/components/vezbaCheckbox.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+const vezbaCheckbox = () => {
+    const [check, setCheck] = useState({
+        label: "Pretplata na newsletter",
+        
+    })
+}
